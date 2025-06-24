7bf1ea90d14a5376c8794823dc5a65826d02ad02
Ammon <44215158+NightMarethyu@users.noreply.github.com>
2025-06-24 13:37:30 -1000

    Feat: Add working Docker configuration for MERN

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const testRouter = require("./src/routes/testapi");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.error(err));
+
+app.use("/testapi", testRouter);
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: req.app.get("env") === "development" ? err : {},
+  });
+});
+
+module.exports = app;
